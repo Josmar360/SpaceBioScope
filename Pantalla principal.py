fe8869189a67cce97ef2dfe76818d84446438988
Aquiles <apinao2000@alumno.ipn.mx>
2024-10-05 14:38:37 -0600

    chore: Add menu and catalog screens

--- Pantalla principal.py
+++ Pantalla principal.py
@@ -12,6 +12,12 @@ pygame.display.set_caption("SpaceBioScope")
 # Colores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+
+# Estados de la pantalla
+PANTALLA_MENU = 0
+PANTALLA_CATALOGO = 1
+pantalla_actual = PANTALLA_MENU
 
 # Fuente y tamaño
 font_size = 64
@@ -24,6 +30,24 @@ def draw_text(text, font, color, surface, x, y):
     textrect.center = (x, y)
     surface.blit(textobj, textrect)
 
+# Función para manejar la pantalla del menú
+def menu_screen():
+    screen.fill(WHITE)
+    draw_text("SpaceBioScope", font, BLACK, screen, screen_width // 2, screen_height // 3)
+    
+    # Dibuja el botón
+    button_rect = pygame.Rect(screen_width // 2 - 500, screen_height // 1.3 - 40, 300, 80)
+    pygame.draw.rect(screen, GREEN, button_rect)
+    draw_text("Catálogo", font, BLACK, screen, screen_width // 2- 400, screen_height // 1.3)
+
+    return button_rect  # Devuelve el rectángulo del botón
+
+# Función para manejar la pantalla del catálogo
+def catalog_screen():
+    screen.fill(WHITE)
+    draw_text("Catalog", font, BLACK, screen, screen_width // 2, screen_height // 3)
+    draw_text("[Esc]", font, BLACK, screen, screen_width // 2, screen_height // 2)
+
 # Bucle principal
 while True:
     # Maneja eventos
@@ -31,12 +55,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if pantalla_actual == PANTALLA_CATALOGO and event.key == pygame.K_ESCAPE:  # Regresa al menú
+                pantalla_actual = PANTALLA_MENU
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pantalla_actual == PANTALLA_MENU:
+                mouse_pos = event.pos  # Obtiene la posición del mouse
+                if button_rect.collidepoint(mouse_pos):  # Verifica si se hizo clic en el botón
+                    pantalla_actual = PANTALLA_CATALOGO
 
-    # Rellena la pantalla con blanco
-    screen.fill(WHITE)
-
-    # Dibuja el texto en el centro de la pantalla
-    draw_text("SpaceBioScope", font, BLACK, screen, screen_width // 2, screen_height // 2)
+    # Renderiza la pantalla actual
+    if pantalla_actual == PANTALLA_MENU:
+        button_rect = menu_screen()  # Guarda el rectángulo del botón
+    elif pantalla_actual == PANTALLA_CATALOGO:
+        catalog_screen()
 
     # Actualiza la pantalla
     pygame.display.flip()
